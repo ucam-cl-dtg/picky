dd5d5a9bf8297d3cdff62e5a2867c67d510c6c60
Daniel Hintze <daniel@hintze-it.de>
2017-10-02 17:00:06 +0200

    Logging + Number of workers

--- picky-client/src/main/java/uk/ac/cam/cl/dtg/picky/client/binding/DatasetBinding.java
+++ picky-client/src/main/java/uk/ac/cam/cl/dtg/picky/client/binding/DatasetBinding.java
@@ -1,38 +1,17 @@
 package uk.ac.cam.cl.dtg.picky.client.binding;
 
-/*
- * #%L
- * Picky
- * %%
- * Copyright (C) 2015 Daniel Hintze <dh526@cl.cam.ac.uk>
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import java.util.List;
 
+import com.google.common.base.Strings;
+
+import de.ecclesia.kipeto.repository.CachedReadingStrategy;
+import de.ecclesia.kipeto.repository.ReadingRepository;
 import javafx.application.Platform;
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import uk.ac.cam.cl.dtg.picky.dataset.Dataset;
 import uk.ac.cam.cl.dtg.picky.dataset.FileEntry;
-
-import com.google.common.base.Strings;
-
-import de.ecclesia.kipeto.repository.CachedReadingStrategy;
-import de.ecclesia.kipeto.repository.ReadingRepository;
 
 public class DatasetBinding extends AsyncBinding<Dataset> implements IStatusProvider {
 
@@ -108,8 +87,15 @@ public class DatasetBinding extends AsyncBinding<Dataset> implements IStatusProv
 				.mapToLong(f -> f.getBlocks().size())
 				.average().getAsDouble();
 
+		long numberOfEntries = files.stream()
+				.flatMap(f -> f.getBlocks().stream())
+				.flatMap(b -> b.getChunks().stream())
+				.mapToLong(c -> c.getEntries())
+				.sum();
+
 		System.out.println("Dataset:" + dataset.getContentDescription());
 		System.out.println("Number of Chunks:" + numberOfChunks);
+		System.out.println("Number of Entries:" + numberOfEntries);
 		System.out.println("Avg number of blocks: " + avgBlocks);
 
 	}
